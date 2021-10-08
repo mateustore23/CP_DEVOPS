166a845e810372e70f526e7f992a58438fbc1ece
mateustore23 <73617467+mateustore23@users.noreply.github.com>
2021-10-08 01:41:51 -0300

    Add files via upload

---
+++ CP_DEVOPS/src/A.java
@@ -0,0 +1,10 @@
+
+public class A {
+	public static void main(String[] args) {
+		int teste = 1;
+		System.out.println("Sequência de números até o 10:");
+		for (int i = 0; i <10; i++) {
+			System.out.println("Número " + (teste+i));
+		}
+	}
+}
